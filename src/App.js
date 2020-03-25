a9567b5628e5e8a0a9783e429ba3f76fc8a40973
Norihito Yamamoto <norihitoyamamoto@NorihitonoMacBook.local>
2020-03-25 13:18:02 +0900

    modify

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import {DataSearch,ReactiveBase,ReactiveList,ResultList,SelectedFilters} from '@appbaseio/reactivesearch';
+import './CityRank.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const {ResultListWrapper} = ReactiveList;
+
+class App extends Component {
+    componentDidMount() {
+        document.title = "City Population Rank"
+    }
+    render() {
+        return (
+            <div className = "main-class">
+                <ReactiveBase
+                    app = "cityindex"
+                    url = "https://search-bimilist2-elubdsi3x4beh3gb6tcbdxd72u.ap-northeast-1.es.amazonaws.com"
+                >
+                    <DataSearch
+                        componentId = "search-component"
+                        dataField = {["countryname"]}
+                        title = "国名を入れてください"
+                        placeholder = "例：日本"
+                        showIcon={false}
+                        className="search-class"
+                    />
+                    <ReactiveList
+                        componentId = "list-component"
+                        pagination = {true}
+                        size = {10}
+                        react = {{
+                            "and": ["search-component"]
+                        }}
+                        sortOptions={[
+                            {label: "ランク", dataField: "rank", sortBy: "asc"}
+                        ]}
+                        className = "list-class"
+                        innerClass={{
+                            button: "button-innerclass",
+                        }}
+                    >
+                        {({data, error, loading}) => (
+                            <ResultListWrapper>
+                                {
+                                    data.map(item => (
+                                        <ResultList key = {item._id} className = "item-class">
+                                            <ResultList.Content>
+                                                <ResultList.Title
+                                                    dangerouslySetInnerHTML = {{
+                                                        __html: item.cityname
+                                                    }}
+                                                />
+                                                <ResultList.Description>
+                                                    <div className="description-class">
+                                                        <div className="countryname-class"> {item.countryname} </div>
+                                                        <div className="population-class"> {item.population}万人 </div>
+                                                    </div>
+                                                </ResultList.Description>
+                                            </ResultList.Content>
+                                        </ResultList>
+                                    ))
+                                }
+                            </ResultListWrapper>
+                        )}
+                    </ReactiveList>
+                </ReactiveBase>
+            </div>
+        );
+    }
 }
 
 export default App;
